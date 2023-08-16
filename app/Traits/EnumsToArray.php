2e03fd2dabba64d9bf1bd3e982cbb7508ac94e79
Aldiama Hari Octavian <aldiamahari16@gmail.com>
2023-08-16 12:07:24 +0700

    update: Traits Enums To Array Update

--- app/Traits/EnumsToArray.php
+++ app/Traits/EnumsToArray.php
@@ -4,19 +4,45 @@ namespace App\Traits;
 
 trait EnumsToArray
 {
-  public static function toArray()
+  public static function toArray(...$indexes)
   {
-    return array_map(
-      fn (self $enum) => $enum->value,
-      self::cases()
-    );
+    $cases = self::cases();
+    $selectedCases = [];
+
+    foreach ($indexes as $index) {
+      if (array_key_exists($index, $cases)) {
+        $selectedCases[] = $cases[$index]->value;
+      }
+    }
+
+    if (empty($selectedCases)) {
+      return array_map(
+        fn (self $enum) => $enum->value,
+        $cases
+      );
+    }
+
+    return $selectedCases;
   }
 
-  public static function toValidation()
+  public static function toValidation(...$indexes)
   {
-    return 'in:' . implode(',', array_map(
-      fn (self $enum) => $enum->value,
-      self::cases()
-    ));
+    $cases = self::cases();
+    $selectedCases = [];
+
+    foreach ($indexes as $index) {
+      if (array_key_exists($index, $cases)) {
+        $selectedCases[] = $cases[$index]->value;
+      }
+    }
+
+    if (empty($selectedCases)) {
+      return 'in:' . implode(',', array_map(
+        fn (self $enum) => $enum->value,
+        $cases
+      ));
+    }
+
+    return 'in:' . implode(',', $selectedCases);
   }
 }
